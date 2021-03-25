e695b9cffbf02798e831118f4f881369ed8cfc71
Rafael Oliveira <oliveira.rafa85@gmail.com>
2021-03-25 16:13:56 -0300

    new url

--- src/configs/environments/environment.prod.ts
+++ src/configs/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://mycareer-api-alter-dot-my-career-303723.uc.r.appspot.com'
+    apiUrl: 'https://mycareer-api-dot-my-career-303723.uc.r.appspot.com'
     // apiUrl: '${process.env.API_BASE_URL}'
 };
